bd294ec422b7d210841034afc8cbf7e49c9b0dcc
t1ebetter <33831450+t1ebetter@users.noreply.github.com>
2022-10-28 10:56:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -61,9 +61,9 @@ SWITCH: {
       // 想要发送的人的名字
       name: '软软',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o-Opv53Z9u4TNoOHezBiFBcVNQqY',
+      id: '6fde47df1da744cda045b602f18228d3',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'cx-tA5WH2bPDivA8772nfqb9DFYWz4TN_bdEoTmzp_s',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-23',
       festivals: [
