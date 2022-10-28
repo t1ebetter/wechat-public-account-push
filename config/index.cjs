2b40715eb6db4befbfdfd4a70e93f48368814e5b
t1ebetter <33831450+t1ebetter@users.noreply.github.com>
2022-10-28 12:37:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -69,22 +69,22 @@ SWITCH: {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '软软', year: '1997', date: '10-23',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '李四', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+         // type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        {// keyword: 'love_day', date: '2022-09-08' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        {// keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
