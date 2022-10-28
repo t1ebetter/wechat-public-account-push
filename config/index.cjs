8947f6ddd8cf564b5bfa95c6e878f2cfa24f7157
t1ebetter <33831450+t1ebetter@users.noreply.github.com>
2022-10-28 10:37:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '6fde47df1da744cda045b602f18228d3',
+      id: 'o-Opv53Z9u4TNoOHezBiFBcVNQqY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
